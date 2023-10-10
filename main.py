d2e51f7c59a00e2d23ce538c17c8686db655e585
jennyakateva <27323508-jennyakateva@users.noreply.replit.com>
2023-10-10 21:57:39 +0000

    create currency rate api

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
+from flask import Flask, jsonify
+
+from bs4 import BeautifulSoup
 import requests
-import os
 
-city_name = 'Denver'
-state_code = 'CO'
-country_code = 'US'
-API_key = os.environ['OPENWEATHERAPP_API']
 
-url = f"api.openweathermap.org/data/2.5/forecast?q={city_name},{state_code},{country_code}&appid={API_key}"
+def get_currency(in_currency, out_currency):
+  url = "https://www.oanda.com/currency-converter/en/?from={in_currency}&to={out_currency}&amount=1"
+  content = requests.get(url).text
+  soup = BeautifulSoup(content, 'html.parser')
+  rate = soup.find('span', class_="ccOutputRslt").get_text()
+  return rate
 
-url_city = f"https://api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml&appid={API_key}?"
 
-r = requests.get(url_city)
+app = Flask(__name__)
 
-content = r.json()
 
-print(content)
+@app.route('/')
+def home():
+  return "<h1>Currency Rate API</h1> <p> Example URL: /api/v1/usd-eur</p>"
+
+
+@app.route('/api/v1/<in_currency>-<out_currency>')
+def api(in_currency, out_currency):
+
+  rate = get_currency(in_currency, out_currency)
+
+  rate_dict = {
+      'input_currency': in_currency,
+      'out_currency': out_currency,
+      'rate': rate
+  }
+
+  return jsonify(rate_dict)
+
+
+#app.run(host="0.0.0.0")
