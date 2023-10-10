83dd4e4cf003f1ba4e723fc9860fdad8254e4ef7
jennyakateva <27323508-jennyakateva@users.noreply.replit.com>
2023-10-10 21:22:06 +0000

    update secrets

--- read_news_api.py
+++ read_news_api.py
@@ -1,7 +1,10 @@
 import requests
+import os
+
+NEWS_API = os.environ['NEWS_API']
 
 data = requests.get(
-    'https://newsapi.org/v2/everything?qInTitle=united%20states&from=2023-10-01&to=2023-10-02&sortBy=popularity&language=en&apiKey=853e0c4188634ecfb962f0de8c7be1ce'
+    f'https://newsapi.org/v2/everything?qInTitle=united%20states&from=2023-10-01&to=2023-10-02&sortBy=popularity&language=en&apiKey={NEWSAPI}'
 )
 
 content = data.json()
