48e7faea43891b38352b9ad10262cf40cebfff8f
jennyakateva <27323508-jennyakateva@users.noreply.replit.com>
2023-10-11 17:59:33 +0000

    rename

---
+++ build_currency_rate_api.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify
+
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_currency(in_currency, out_currency):
+  url = "https://www.oanda.com/currency-converter/en/?from={in_currency}&to={out_currency}&amount=1"
+  content = requests.get(url).text
+  soup = BeautifulSoup(content, 'html.parser')
+  rate = soup.find('span', class_="ccOutputRslt").get_text()
+  return rate
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+  return "<h1>Currency Rate API</h1> <p> Example URL: /api/v1/usd-eur</p>"
+
+
+@app.route('/api/v1/<in_currency>-<out_currency>')
+def api(in_currency, out_currency):
+
+  rate = get_currency(in_currency, out_currency)
+
+  rate_dict = {
+      'input_currency': in_currency,
+      'out_currency': out_currency,
+      'rate': rate
+  }
+
+  return jsonify(rate_dict)
+
+
+#app.run(host="0.0.0.0")
--- read_news_api.py
+++ read_news_api.py
@@ -4,7 +4,7 @@ import os
 NEWS_API = os.environ['NEWS_API']
 
 data = requests.get(
-    f'https://newsapi.org/v2/everything?qInTitle=united%20states&from=2023-10-01&to=2023-10-02&sortBy=popularity&language=en&apiKey={NEWSAPI}'
+    f'https://newsapi.org/v2/everything?qInTitle=united%20states&from=2023-10-01&to=2023-10-02&sortBy=popularity&language=en&apiKey={NEWS_API}'
 )
 
 content = data.json()
