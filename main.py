6c9dd7e3e61f96836c801ae3465427109af4e3e7
evgeniya1 <jennyakateva@gmail.com>
2023-10-13 22:19:30 +0000

    add dynamic web scraping read

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import time
 
 #for local IDE
 #from selenium.webdriver.chrome.service import Service
 #service = Service("path to chrome driver")
+
 
 def get_driver():
   #set options to make browsing easier
@@ -17,16 +19,24 @@ def get_driver():
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
   options.add_argument("disable-blink-features=AutomationControlled")
 
-  driver = webdriver.Chrome(options=options) #service=service
+  driver = webdriver.Chrome(options=options)  #service=service
   driver.get("http://automated.pythonanywhere.com")
 
   return driver
 
 
+def clean_text(text):
+  return float(text.split(": ")[1])
+
+
 def main():
   driver = get_driver()
-  element = driver.find_element(By.XPATH, "/html/body/div[1]/div/h1[1]")
-  return element.text
+  time.sleep(2)
+  element_static = driver.find_element(By.XPATH, "/html/body/div[1]/div/h1[1]")
+  element_dynamic = driver.find_element(By.XPATH,
+                                        "/html/body/div[1]/div/h1[2]")
+
+  return element_static.text, clean_text(element_dynamic.text)
 
 
 print(main())
