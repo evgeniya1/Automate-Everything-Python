53a166c9f021a64d72697ef29dd2b0794cfe9c66
evgeniya1 <jennyakateva@gmail.com>
2023-10-18 16:31:32 +0000

    add flask app calculate volume

--- main.py
+++ main.py
@@ -1,38 +1,19 @@
-from flask import Flask, jsonify
-
-from bs4 import BeautifulSoup
-import requests
-
-
-def get_currency(in_currency, out_currency):
-  url = f"https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1"
-  content = requests.get(url).text
-  soup = BeautifulSoup(content, "html.parser")
-  rate_text = soup.find("span", class_="ccOutputRslt").get_text()
-  rate = float(rate_text.split(' ')[0])
-  return rate
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
-  return "<h1>Currency Rate API</h1> <p> Example URL: /api/v1/usd-eur</p>"
+  return render_template('index.html')
 
-
-@app.route('/api/v1/<in_currency>-<out_currency>')
-def api(in_currency, out_currency):
-
-  rate = get_currency(in_currency, out_currency)
-
-  rate_dict = {
-      'input_currency': in_currency,
-      'out_currency': out_currency,
-      'rate': rate
-  }
-
-  return jsonify(rate_dict)
-
+@app.route('/', methods = ['POST'])
+def home_post():
+  
+  length = float(request.form['length'])
+  height = float(request.form['height'])
+  width = float(request.form['width'])
+  output = length*height*width
+  
+  return render_template('index.html', output_=output, length_=length, height_=height, width_=width)
 
 app.run(host="0.0.0.0")
